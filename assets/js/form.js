03ddc004d343abd6e5b12497c50a04b21ecfb916
Marinofull <intmarinoreturn0@gmail.com>
2015-04-15 02:09:30 -0300

    ajustando css

--- assets/js/form.js
+++ assets/js/form.js
@@ -15,18 +15,51 @@ function insertParada () {
 
     name = document.getElementById("nameForm").value;
     email = document.getElementById("emailForm").value;
-    purpouse = document.getElementById("optForm").value;
+    purpouse = document.getElementById("optForm").ELEMENT_NODE;
     text = document.getElementById("textForm").value;
     checkbox = document.getElementById("checkboxForm").value;
 
-    var c = "\<div>\n\
-        name<p>"+name+"</p>\n\
-        em<p>"+email+"</p>\n\
-        pu<p>"+purpouse+"</p>\n\
-        t<p>"+text+"</p>\n\
-        ch<p>"+checkbox+"</p>\n\
-    </div>";
+    purpouseString = translatePurpouse(purpouse);
+    checkboxString = translateCheckbox(checkbox);
 
-    document.getElementById('trashArea').innerHTML += c;
+    var newChild = "\<div id=\"elementResult\">\n\
+        <div id=\"elementDivTittle\" >\n\
+            <p id=\"elementResultTitles\" ><span>Nome: </span>"+name+"</p>\n\
+            <p id=\"elementResultTitles\" ><span>E-mail: </span>"+email+"</p>\n\
+            <p id=\"elementResultTitles\" ><span>Propósito: </span>"+purpouseString+"</p>\n\
+        </div>\n\
+        <div id=\"elementDivMessage\" >\n\
+            <p id=\"elementResultMessage\" ><span>Mensagem: </span>"+text+"</p>\n\
+        </div>\n\
+        <p id=\"elementResultTitles\" ><span>Acept: </span>"+checkboxString+"</p>\n\
+    </div><hr>";
 
+    document.getElementById('trashArea').innerHTML += newChild;
+}
+
+function translatePurpouse (value) {
+    var s;
+
+    switch (value){
+        case 1:
+            s = "Contato Profissional";
+            break;
+        case 2:
+            s = "Consultoria";
+            break;
+        case 3:
+            s = "Palestra";
+            break;
+        case 4:
+            s = "Cumê Água";
+            break;
+        default:
+            break;
+    }
+
+    return s;
+}
+
+function translateCheckbox (value) {
+    return (value)? "true" : "false";
 }
