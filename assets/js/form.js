a84ea8daaae16c8d206967cfedf00cbcb18c85b2
Marinofull <intmarinoreturn0@gmail.com>
2015-04-21 21:23:54 -0300

    verificaçoes e formularios dinamicos

--- assets/js/form.js
+++ assets/js/form.js
@@ -6,35 +6,44 @@
     Os scripts devem estar em um arquivo externo.
 */
 
+var count = 0;
+
 function insertParada () {
     // var e = document.createElement("p");
     // e.value= "texto";
     // document.getElementById('trashArea').innerHTML = "texto";
 
-    var name, email, purpouse, text, checkbox;
+    var name, email, purpouse, text, checkbox, date;
 
     name = document.getElementById("nameForm").value;
     email = document.getElementById("emailForm").value;
     purpouse = document.getElementById("optForm").ELEMENT_NODE;
     text = document.getElementById("textForm").value;
     checkbox = document.getElementById("checkboxForm").value;
+    date = new Date();
 
     purpouseString = translatePurpouse(purpouse);
     checkboxString = translateCheckbox(checkbox);
+    // tam = document.getElementById('trashArea').length;
+    // tam = tam*3 + 1;
 
-    var newChild = "\<div id=\"elementResult\">\n\
-        <div id=\"elementDivTittle\" >\n\
-            <p id=\"elementResultTitles\" ><span>Nome: </span>"+name+"</p>\n\
-            <p id=\"elementResultTitles\" ><span>E-mail: </span>"+email+"</p>\n\
-            <p id=\"elementResultTitles\" ><span>Propósito: </span>"+purpouseString+"</p>\n\
+    var newChild = "\<p><a id=\"elementChildDate\" href='#trashArea' onclick=\"hideStuff('elementResult"+count+"', this)\">Contato de : "+date+"</a></p>\n\
+    <div id='elementResult"+count+"' class=\"elementResult\">\n\
+        <div class=\"elementDivTittle\" >\n\
+            <p class=\"elementResultTitles\" ><span>Nome: </span>"+name+"</p>\n\
+            <p class=\"elementResultTitles\" ><span>E-mail: </span>"+email+"</p>\n\
+            <p class=\"elementResultTitles\" ><span>Propósito: </span>"+purpouseString+"</p>\n\
         </div>\n\
-        <div id=\"elementDivMessage\" >\n\
-            <p id=\"elementResultMessage\" ><span>Mensagem: </span>"+text+"</p>\n\
+        <div class=\"elementDivMessage\" >\n\
+            <p class=\"elementResultMessage\" ><span>Mensagem: </span>"+text+"</p>\n\
         </div>\n\
-        <p id=\"elementResultTitles\" ><span>Acept: </span>"+checkboxString+"</p>\n\
+        <p class=\"elementResultTitles elementCheckbox\" ><span>Acept: </span>"+checkboxString+"</p>\n\
     </div><hr>";
 
     document.getElementById('trashArea').innerHTML += newChild;
+    // document.getElementsByClassName('trashArea').innerHTML += newChild; It don't works on Firefox
+
+    count++
 }
 
 function translatePurpouse (value) {
@@ -61,5 +70,16 @@ function translatePurpouse (value) {
 }
 
 function translateCheckbox (value) {
-    return (value)? "true" : "false";
+    return (value )? "true" : "false";
+}
+
+function showStuff(id, me) {
+    document.getElementById(id).style.display = 'block';
+    var attr = "hideStuff('"+id+"', this); return false;";
+    me.setAttribute("onclick", attr);
+}
+function hideStuff(id, me) {
+    document.getElementById(id).style.display = 'none';
+    var attr = "showStuff('"+id+"', this); return false;";
+    me.setAttribute("onclick", attr);
 }
